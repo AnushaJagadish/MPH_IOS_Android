f0960b40133b48bde74bd5af638f3f4e86cacb9d
Revathy chandran <53893307+Revathychandran47@users.noreply.github.com>
2021-08-31 15:05:08 +0530

    Add files via upload

--- src/test/java/Runner_File/TestSuite.java
+++ src/test/java/Runner_File/TestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = {"src/test/resources/features"},glue= {"Stepdefinition"})
-public class  TestSuite {
+        features = "src/test/resources/features/03_Signup_positive_scenario.feature",glue= {"Stepdefinition"})
+public class TestSuite {
 
 }
