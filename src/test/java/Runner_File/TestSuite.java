f16f166ea50cd19b7797212f8f3a3b105affc77d
AnushaJagadish <anusha.jagadish@mpoweredhealth.com>
2022-06-09 16:05:36 +0530

    New framework

--- src/test/java/Runner_File/TestSuite.java
+++ src/test/java/Runner_File/TestSuite.java
@@ -6,8 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        plugin = {"pretty"},
-        features = "src/test/resources/features/01_Landing_positive_scenario.feature",glue= {"Stepdefinition"})
+        publish = true,
+        plugin = {"pretty","html:target/site/serenity/Report.html"},
+        features = "src/test/resources/features/01_Landing_positive_scenario.feature",
+        glue= {"Stepdefinition"})
 public class TestSuite {
 
 }
